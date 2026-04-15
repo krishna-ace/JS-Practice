560caa351389db3b5e23fc60e6d6952a42658d17
krishna-ace <kp75617@gmail.com>
2026-04-15 15:52:58 +0530

    Add files via upload

---
+++ checkPalindrome.js
@@ -0,0 +1,17 @@
+function isPalindrome(s) {
+  let left = 0
+  let right = s.length - 1
+
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false
+    }
+    left++
+    right--
+  }
+
+  return true
+}
+
+console.log(isPalindrome("madam"))
+console.log(isPalindrome("hello"))
